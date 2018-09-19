9f7a76a05159df642b08b1781388f2914334121a
Juan Diego Restrepo <jurest74@eafit.edu.co>
2018-09-19 00:55:40 -0500

    avances
    incluyendo cambio del icono estandar

--- follow/follow.Android/MainActivity.cs
+++ follow/follow.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace follow.Droid
 {
-    [Activity(Label = "follow", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "follow", Icon = "@drawable/icono", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)

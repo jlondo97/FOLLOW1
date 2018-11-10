980e4b3d1ce45dde025b675c6911ebd21a63c20d
Juan Diego Restrepo <jurest74@eafit.edu.co>
2018-11-10 14:04:24 -0500

    Actualizacion10112018

---
+++ follow/follow/View/EducadoresView/Estudiantes.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace follow.View.EducadoresView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Estudiantes : ContentPage
+    {
+        public Estudiantes()
+        {
+            InitializeComponent();
+
+            var TapProfile = new TapGestureRecognizer();
+            TapProfile.Tapped += Tap_Profile;
+            Tarea.GestureRecognizers.Add(TapProfile);
+
+        }
+
+        void Tap_Profile(object sender, EventArgs args)
+        {
+
+            Application.Current.MainPage.Navigation.PushAsync(new VistaTareasEducador());
+
+        }
+    }
+}
--- follow/follow/View/EducadoresView/VistaTareasEducador.xaml.cs
+++ follow/follow/View/EducadoresView/VistaTareasEducador.xaml.cs
@@ -11,15 +11,46 @@ using Xamarin.Forms.Xaml;
 
 namespace follow.View.EducadoresView
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class VistaTareasEducador : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VistaTareasEducador : ContentPage
+    {
         public VistaTareasEducador()
         {
-            InitializeComponent();            
+
+            InitializeComponent();
+          
+
+            var TapProfile = new TapGestureRecognizer();
+            TapProfile.Tapped += Tap_Profile;
+            Profile.GestureRecognizers.Add(TapProfile);
+
+
+        }
+
+        public string codigo_Grupo()
+        {
+            var longitud = 6;
+            string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < longitud--)
+            {
+                res.Append(caracteres[rnd.Next(caracteres.Length)]);
+            }
+            return res.ToString();
+        }
+
+        void Tap_Profile(object sender, EventArgs args)
+        {
+
+            Application.Current.MainPage.Navigation.PushAsync(new Estudiantes());
+
+        }
+
+                    
             
 		}
 
        
+
     }
-}

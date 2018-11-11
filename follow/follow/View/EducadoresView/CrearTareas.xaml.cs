dcda8fc25113aeaeae7b474eda9c1fb9cae4faf0
jlondo97 <42972979+jlondo97@users.noreply.github.com>
2018-11-11 01:37:43 -0500

    agregar tarea vistas

---
+++ follow/follow/View/EducadoresView/CrearTareas.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace follow.View.EducadoresView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CrearTareas : ContentPage
+    {
+        public CrearTareas()
+        {
+            InitializeComponent();
+        }
+
+        private void Guardar_Tarea (object sender, EventArgs e)
+        {
+
+        }
+    }
+}

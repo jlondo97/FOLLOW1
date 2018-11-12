604e5c1c6bc4442acc2014bde8d85ec804a475ae
jlondo97 <42972979+jlondo97@users.noreply.github.com>
2018-11-12 08:43:44 -0500

    ,,,

--- follow/follow/Infrastructure/AccesoDatosTarea.cs
+++ follow/follow/Infrastructure/AccesoDatosTarea.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using follow.View.EducadoresView;
+using System.Data.SqlClient;
 
 namespace follow.Infrastructure
 {
-    class AccesoDatosTarea
+   public class AccesoDatosTarea
     {
+        public static int IngresarTarea(string Nombre, string Descripcion , string Fecha_inicio,
+            string Fecha_final,string Grupo)
+        {
+            SqlCommand comando = MetodoDatos.CrearComandoProc("insTarea");
+
+            comando.Parameters.AddWithValue("@Nombre", Nombre);
+            comando.Parameters.AddWithValue("@Fecha_inicio", Fecha_inicio);
+            comando.Parameters.AddWithValue("@Fecha_final", Fecha_final);
+            comando.Parameters.AddWithValue("@Descripcion", Descripcion);
+            comando.Parameters.AddWithValue("@Grupo", Grupo);
+
+            return MetodoDatos.EjecutarComando(comando);
+        }
     }
 }
